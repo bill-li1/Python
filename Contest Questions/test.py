159a6ded9a9b3400c3bd38a36f221d3f3c76242d
Bill Li <billli@gmail.com>
2021-07-21 16:56:49 -0400

    new problems

--- Contest Questions/test.py
+++ Contest Questions/test.py
@@ -1,3 +1,32 @@
-a = int(input())
-for i in range(a):
-    print(i)
+def binary_search(L, target):
+    beginning = 0
+    end = len(L) - 1
+    while beginning <= end:
+        mid = (beginning + end) // 2
+        print(mid)
+        if L[mid] == target:
+            return True
+        elif L[mid] > target:
+            end = mid
+        else:
+            beginning = mid + 1
+    return False
+
+
+def sum_pair(L):
+    total = 0
+    for i in range(len(L)):
+        total += L[i]
+    if total % 2 != 0:
+        return None
+    total //= 2
+    for i in range(len(L)):
+        missing = total - L[i]
+        if binary_search(L, missing):
+            if L[i] > missing:
+                return [missing, L[i]]
+            else:
+                return [L[i], missing]
+    return None
+
+print(sum_pair([1, 10, 5, 2, 4, 8]))
